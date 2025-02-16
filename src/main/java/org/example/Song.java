b4b02e4a2b571fba1a7551a3f836e000a65d888f
Jake Berryman <59934842+ProGoofster@users.noreply.github.com>
2025-02-15 20:20:57 -0600

    implement song

---
+++ src/main/java/org/example/Song.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Song
+{
+    private String title;
+    private String artist;
+    private double length;
+
+    // Constructor to initialize the Song object
+    public Song(String title, String artist, double length) {
+        this.title = title;
+        this.artist = artist;
+        this.length = length;
+    }
+}
